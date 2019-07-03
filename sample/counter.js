fa33cb878f03d75e56f062ab6f1b31d7ea8781a7
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2019-07-03 13:59:56 +0900

    react

--- sample/counter.js
+++ sample/counter.js
@@ -2,8 +2,38 @@ class Counter extends React.Component {
     render() {
         return (
             <div>
-                0
+                {this.props.display}
             </div>
         );
     }
 }
+
+class CounterParent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            count: 0
+        };
+
+        this.increase = this.increase.bind(this);
+    }
+    increase() {
+        this.setState({
+            count: this.state.count+1
+        })
+    }
+    render() {
+        return (
+            <div>
+                <Counter display={this.state.count}/>
+                <button onClick={this.increase}> + </button>
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <div>
+        <CounterParent/>
+    </div>,
+    document.querySelector("#container")
+);
