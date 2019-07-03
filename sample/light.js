a43f07573c983ec52d5b0eaddc4dd3267504cc4f
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2019-07-03 11:02:11 +0900

    react

--- sample/light.js
+++ sample/light.js
@@ -1,16 +1,29 @@
 class LightningCounter extends React.Component {
-    
+
     /* 초기 상태 값 설정 */
     constructor(props) {
         super(props);
 
         this.state = {
-            strikes: 0
+            strikes: 0,
+            msg: "번개횟수"
         };
+        /* bind : 컴포넌트에 이벤트 연결 */
+        this.timerTick = this.timerTick.bind(this);
+    }
+    timerTick() {
+       /*  setState : 객체의 값을 갱신할 수 있게 해준다. */
+        this.setState({
+            strikes: this.state.strikes + 100
+        });
+    }
+   /*  componentDidMount : 컴포넌트가 렌더링(마운트)된 후에 실행 */
+    componentDidMount() {   //render 뒤에 react 엔진이 자동으로 호출
+        setInterval(this.timerTick, 1000);
     }
     render() {
         return (
-            <h1>{this.state.strikes}</h1>
+            <h1>{this.state.msg} {this.state.strikes}</h1>
         );
     }
 }
