5e0898cf5ac517f980f21c203e20132a8a4a3519
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2019-09-01 12:32:39 +0900

    react_2

--- movie_app_2019/src/index.js
+++ movie_app_2019/src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/* react Application은 하나의 Component만 Reandering 가능 */
 ReactDOM.render(<App />, document.getElementById('root'));
