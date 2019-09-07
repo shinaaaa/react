fa75dcdea8abc826c1b1c9305a2fbe5d5043bf7e
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2019-09-07 13:23:35 +0900

    setTimeout

--- movie_app_2019/src/App.js
+++ movie_app_2019/src/App.js
@@ -1,36 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class App extends React.Component {
-
   state = {
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
+    isLoading: true
   };
   componentDidMount() {
-    console.log("componentDidMount");
-  }
-  componentDidUpdate() {
-    console.log("componentDidUpdate");
-  }
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 5000);
   }
   render() {
-    console.log("render");
-    
-    return (
-      <div>
-        <h1>The Number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "we are ready"}</div>
   }
 }
 
